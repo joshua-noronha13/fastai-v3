c67f61165b26d9915686d461778ab068b89366b8
Edwin Clement <edwinclement08@gmail.com>
2019-03-29 02:16:43 +0530

    changed the pth again

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/open?id=11xdboxvlgPGtde4JN-meeTVYe0z-e3PR'
+export_file_url = 'https://ucf2988ac1f02d26678a0290ec4f.dl.dropboxusercontent.com/cd/0/get/Ad-uFPuWwHPwM2v5n9QUkih5F6Qi9dR8xlykNMPYun-EAehUi-2ViHSBzg5mHqqPMxw2yolcjihWoemngQdC0STE7_gfGuL0Y6nrPTafshWVCuewNWKEBMD8oG0taT4TMsE/file?dl=1#'
 export_file_name = 'foodclassify.pth'
 
 classes = ['food', 'notfood']
